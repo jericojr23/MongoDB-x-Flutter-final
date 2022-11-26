2645211374b11a34e1b55923e3f53a0ba9cf00ae
jjjjjjjjjj24 <jericodlsrys@gmail.com>
2022-11-26 11:05:41 +0800

    First Commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const connectDB = require('./config/db')
+const passport = require('passport')
+const bodyParser = require('body-parser')
+
+connectDB()
+
+const app = express()
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, console.log(`Server runing in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
+
